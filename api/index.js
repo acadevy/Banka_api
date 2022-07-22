02a3b1de4935847a3d999a99ae39092579f2d158
acadevy <rashyolat@gmail.com>
2022-07-22 10:07:32 +0100

    setting up the project

---
+++ api/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+// const app = require('./app');
+// require("dotenv").config();
+
+
+const port = process.env.PORT || 6000;
+
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
+
+
