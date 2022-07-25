077904b4fe0a6c40e824c2ef5df4f5f5f47a3225
acadevy <rashyolat@gmail.com>
2022-07-25 22:45:43 +0100

    worked on the credit account feature

--- api/controllers/transaction_controller.js
+++ api/controllers/transaction_controller.js
@@ -24,3 +24,28 @@ exports.debit_account = async(req,res) => {
         res.status(500).json(err.message);
     }
 }
+
+exports.credit_account = async(req,res) => {
+    const { amount,_id } = req.body;
+    const { id } = req.params;
+    try{
+        if (req.user.role !== "super-admin"){
+            return res.status(403).json({message:"You are not permitted"});
+        }
+        else{
+            const account = await Account.findOne({id});
+            const updated_amount = account.account[0].account_balance += amount;
+
+                const updated_account = await Account.findByIdAndUpdate(account._id,
+                    {"$set": {"account.$[element].account_balance": updated_amount.toFixed(2)}},
+                    {new: true, useFindAndModify: false, arrayFilters: [{ "element._id": { $eq:_id}}]}
+                    )
+                    return res.status(200).json(updated_account);
+        
+            }
+       
+        res.status(200).json({message: "You are correct"});
+        }catch(err){
+        res.status(500).json(err.message);
+    }
+}
--- api/routes/transaction.js
+++ api/routes/transaction.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 
-const { debit_account } = require("../controllers/transaction_controller")
+const { debit_account, credit_account } = require("../controllers/transaction_controller")
 const { auth }  = require("../middlewares/auth");
 
 
 router.post('/debit/:id',auth,debit_account);
+router.post('/credit/:id', auth, credit_account);
 
 module.exports = router;
